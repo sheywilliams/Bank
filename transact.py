eb98d022e5719034b21b58030edf6a62af5eb9d1
SheyWilliams <sheywilliams99@gmail.com>
2023-01-25 12:02:41 +0000

    removed the 'while' statement for now

--- transact.py
+++ transact.py
@@ -6,41 +6,35 @@ class trans:
     def __init__(self):
         self.act()
     def act(self):
-        x = 0
-        while x <= 3:
-            q = input("Enter your Bank id >>> ")
-            t = input("Enter your pin >>> ")
-            if q or t :
-                myquery = 'SELECT * FROM User WHERE Bank_id=%s AND Bank_pin=%s'
-                val = (q, t)
-                mycursor.execute(myquery, val)
-                myreg = mycursor.fetchone()
-                mydb.commit()
-                print('Welcome User '+ q)
-                fy = int(input('''
-                Choose option:
-                 1.  Cash Withdrawal 2. Balance Inquiry 3. Transfer 
-                 4. Bill Payment 5. Recent Transactions 
-                 '''))
-                if fy == 1: 
-                    self.one()
-                elif fy == 2:
-                    self.two()
-                elif fy == 3:
-                    self.thr() 
-                elif fy == 4:
-                    self.four()
-                elif fy == 5:
-                    self.five()
-                else:
-                    print('Wrong input')
-                    quit()
-                break           
+        q = input("Enter your Bank id >>> ") 
+        t = input("Enter your pin >>> ")
+        if t :
+            myquery = 'SELECT * FROM User WHERE Bank_id=%s AND Bank_pin=%s'
+            val = (q, t)
+            mycursor.execute(myquery, val)
+            myreg = mycursor.fetchone()
+            mydb.commit()
+            print('Welcome User '+ q)
+            fy = int(input('''
+            Choose option:
+                1.  Cash Withdrawal 2. Balance Inquiry 3. Transfer 
+                4. Bill Payment 5. Recent Transactions 
+                '''))
+            if fy == 1: 
+                self.one()
+            elif fy == 2:
+                self.two()
+            elif fy == 3:
+                self.thr() 
+            elif fy == 4:
+                self.four()
+            elif fy == 5:
+                self.five()
             else:
-                print('Wrong input. Try again, %d tries left' %(3-x)) 
-                x += 1    
+                print('Wrong input')
+                quit()          
         else:
-            print('Your account has been blocked, contact our customer service center for help')  
+            print('Wrong login details, Try again')  
 trans.act(1)
     # def one(self):
 
